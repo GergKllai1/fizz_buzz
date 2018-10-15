21c9a046fb5f3f801bbcfb759029c6fb71a721cd
Greg Kallai <gerg.kallai@gmail.com>
2018-10-15 09:51:00 +0200

    fizz buzz

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,15 @@
+def fizz_buzz(number)
+    if has_zero_remainder?(number,15)
+        "fizzbuzz"
+    elsif has_zero_remainder?(number,3)
+        'fizz'
+    elsif has_zero_remainder?(number,5)
+        'buzz'
+    else
+        number
+    end
+end
+
+def has_zero_remainder?(number,divider)
+    number % divider == 0
+end
